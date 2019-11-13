49a79006888eda6b36d8d44f07b03759f5fc1cc5
Alon Tron <alontron@cs.huji.ac.il>
2019-11-12 23:39:00 -0800

    added alon architecture

--- baseline_cnn.py
+++ baseline_cnn.py
@@ -134,6 +134,56 @@ class ExNet(nn.Module):
         return "ExNet"
 
 
+class TronNet(nn.Module):
+    def __init__(self, num_classes=201):
+        super(Nnet, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(3, 21, 3, stride=1, padding=1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(21, 30, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(30),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3),
+            nn.Conv2d(30, 35, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(35),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(35, 25, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(25),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3),
+            nn.Conv2d(25, 20, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(20),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(20, 15, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(15),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(15, 10, 5, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(10),
+            nn.ReLU(inplace=True),
+        )
+        self.fc = nn.Sequential(
+            nn.Dropout(0.4),
+            nn.Linear(1210, 300),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.4),
+            nn.Linear(300, num_classes)
+        )
+
+        self.train_epoch_losses = []
+        self.val_epoch_losses = []
+
+    def forward(self, input):
+        x = self.main(input)
+        x = x.view(-1, num_flat_features(x))
+        return self.fc(x)
+
+    def __str__(self):
+        return "Nnet"
+
+    def __repr__(self):
+        return "Nnet"
+
+
 class TransferNet(nn.Module):
     def __init__(self, num_classes=201):
         super(TransferNet, self).__init__()
